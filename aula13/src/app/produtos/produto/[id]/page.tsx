91ba2b0c11320d4f2673025a714212444bfc12bf
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-24 23:04:14 -0300

    API com router-hadlers e API aninhada com parametrização. Insomnia.

--- aula13/src/app/produtos/produto/[id]/page.tsx
+++ aula13/src/app/produtos/produto/[id]/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
 
-export default function Produto() {
+export default function Produto({params}:{params:{id:number}}) {
+
+
+
   return (
     <div>
         <h1>Produto</h1>
+        <div>
+            <p>ID: {params.id}</p>
+            
+        </div>
     </div>
   )
 }
