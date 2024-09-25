79d466135caa59b4e1b8e81372649691cafcac98
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-24 23:39:06 -0300

    POST

--- aula13/src/app/api/base-route/[id]/route.tsx
+++ aula13/src/app/api/base-route/[id]/route.tsx
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
 import { TipoLanche } from "@/types";
 
-
-
 export async function GET(request:Request, {params}:{params:{id:number}}) {
     const file = await fs.readFile(process.cwd() + '/src/data/base.json','utf-8');
     const data:TipoLanche[] = JSON.parse(file);
@@ -11,3 +9,5 @@ export async function GET(request:Request, {params}:{params:{id:number}}) {
 
     return NextResponse.json(produto);
 }
+
+
